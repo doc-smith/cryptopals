aaad4226822ba90f4281c8c0762a283406dc83e3
Dmitry Kuznetsov <dimitrij.kuznetsov@gmail.com>
2022-11-20 20:53:46 +0100

    Implemented base64 decoding and added some very basic tests.

--- util/encoding/base64/base64.go
+++ util/encoding/base64/base64.go
@@ -1,9 +1,35 @@
 package base64
 
-const (
-	Padding  byte = '='
-	Alphabet      = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+import (
+	"errors"
+	"fmt"
 )
+
+var LengthError = errors.New("input length must be a multiple of 4")
+
+type InvalidByteError int
+
+func (e InvalidByteError) Error() string {
+	return fmt.Sprintf("invalid input byte at offset %d", int(e))
+}
+
+const (
+	InvalidByteValue byte = 0xff
+	Padding          byte = '='
+	Alphabet              = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+)
+
+var reverseAlphabet [256]byte
+
+func init() {
+	for i := 0; i < len(reverseAlphabet); i++ {
+		reverseAlphabet[i] = InvalidByteValue
+	}
+
+	for i := 0; i < len(Alphabet); i++ {
+		reverseAlphabet[Alphabet[i]] = byte(i)
+	}
+}
 
 func Encode(b []byte) string {
 	var res []byte
@@ -36,4 +62,50 @@ func Encode(b []byte) string {
 	}
 
 	return string(res)
+}
+
+func decodeSegment(s []byte) ([]byte, error) {
+	sextetOffsets := [...]int{18, 12, 6, 0}
+
+	si := 0
+	var chunk uint
+
+	for ; si < 4 && s[si] != Padding; si++ {
+		x := reverseAlphabet[s[si]]
+		if x == InvalidByteValue {
+			return nil, InvalidByteError(si)
+		}
+		chunk |= uint(x) << sextetOffsets[si]
+	}
+
+	for i := si; i < 4; i++ {
+		if s[i] != Padding {
+			return nil, InvalidByteError(i)
+		}
+	}
+
+	res := make([]byte, si-1)
+	for i := 0; i < si-1; i++ {
+		// 16, 8, 0
+		offset := 16 - 8*i
+		res[i] = byte((chunk >> offset) & 0xff)
+	}
+	return res[:], nil
+}
+
+func DecodeString(s string) ([]byte, error) {
+	var res []byte
+	si := 0
+	for ; si < len(s); si += 4 {
+		segment := []byte(s[si : si+4])
+		decoded, err := decodeSegment(segment)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, decoded...)
+	}
+	if len(s)%4 != 0 {
+		return nil, LengthError
+	}
+	return res, nil
 }
---
+++ util/encoding/base64/base64_test.go
@@ -0,0 +1,54 @@
+package base64
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"empty", args{[]byte{}}, ""},
+		{"no-padding", args{[]byte{0x12, 0xaa, 0x7f}}, "Eqp/"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Encode(tt.args.b); got != tt.want {
+				t.Errorf("Encode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDecodeString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"empty", args{""}, nil, false},
+		{"no-padding", args{"Eqp/"}, []byte{0x12, 0xaa, 0x7f}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DecodeString(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DecodeString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DecodeString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
